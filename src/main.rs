6b782f8cb30260992904c2c11395ff7633436a0e
Arun Tomar <arun@tunnelbear.com>
2021-01-05 21:32:10 -0500

    get list of websites from file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+use std::io::prelude::*;
 use futures::future::join_all;
 use tokio::task::JoinHandle;
 use std::time::Duration;
-
+use std::fs::File;
+use std::io::BufReader;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // read the list of websites from a file
-    let alexa_websites = get_websites().expect("something went wrong while getting the list of websites");
-        
+    // let alexa_websites = get_websites().expect("something went wrong while getting the list of websites");
+    let alexa_websites = get_websites_from_file("./sites/CA_sites.txt").expect("something went wrong while getting the list of websites");    
 
     // Create a list of empty tasks
     let mut tasks: Vec<JoinHandle<()>> = Vec::new();
@@ -19,7 +21,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             // Build a client with custom user agent 
             // "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0"
             let client = get_custom_client();    
-            match client.get(url).send().await {
+            match client.get(&url).send().await {
                 Ok(resp) => {
                     // log the site & status
                     println!("Site: {} - Status {:#?}", site, resp.status().as_str());
@@ -34,63 +36,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn get_websites() -> Result<Vec<&'static str>,Box<dyn std::error::Error>> {
-    let websites = vec![
-        "http://localhost",
-        "http://google.com",
-        "http://youtube.com",
-        "http://google.ca",
-        "http://facebook.com",
-        "http://www.reddit.com",
-        "http://amazon.ca",
-        "http://wikipedia.org",
-        "http://twitter.com",
-        "http://netflix.com",
-        "http://live.com",
-        "http://yahoo.com",
-        "http://instagram.com",
-        "http://amazon.com",
-        "http://twitch.tv",
-        "http://kijiji.ca",
-        "http://pornhub.com",
-        "http://linkedin.com",
-        "https://www.td.com",
-        "http://www.royalbank.com",
-        "http://imgur.com",
-        "http://login.microsoftonline.com",
-        "http://livejasmin.com",
-        "http://bestbuy.ca",
-        "http://imdb.com",
-        "http://office.com",
-        "http://www.cbc.ca",
-        "http://T.co",
-        "http://wikia.com",
-        "http://theweathernetwork.com",
-        "http://ebay.ca",
-        "http://narcity.com",
-        "http://scotiabank.com",
-        "http://vice.com",
-        "http://www.rbcroyalbank.com",
-        "http://tumblr.com",
-        "http://paypal.com",
-        "http://www.walmart.ca",
-        "http://pinterest.ca",
-        "http://microsoft.com",
-        "http://apple.com",
-        "http://cnn.com",
-        "http://msn.com",
-        "http://canada.ca",
-        "http://xvideos.com",
-        "http://bing.com",
-        "http://redflagdeals.com",
-        "http://quora.com",
-        "http://github.com",
-        "http://blogspot.com",
-        "http://ebay.com",
-    ];
-    Ok(websites)
-}
-
 fn get_custom_client() -> reqwest::Client {
     let user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0";
     let client = reqwest::Client::builder()
@@ -98,4 +43,14 @@ fn get_custom_client() -> reqwest::Client {
                                 .timeout(Duration::from_secs(30))
                                 .build();
     client.expect("issue creating a custom client")
+}
+
+fn get_websites_from_file(path: &str) -> Result<Vec<String>,Box<dyn std::error::Error>> {
+    let file = File::open(path).expect(&format!("Couldn't open file {}", path));
+    let buffer = BufReader::new(file);
+    let mut websites: Vec<String> = Vec::new();
+    for line in buffer.lines() {
+        websites.push(line.expect("error reading line"));
+    }
+    Ok(websites)
 }
